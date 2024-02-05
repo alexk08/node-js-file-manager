7fcc2e42b7736a6857e767f18bf9166e758c44c3
Aleksandr Krasilnikov <alexander.krasilni@aliexpress.ru>
2024-02-06 00:31:23 +0300

    add sorting in ls command

--- src/ls.mjs
+++ src/ls.mjs
@@ -5,10 +5,22 @@ export const list = async (dirPath) => {
   try {
     const files = await readdir(dirPath, { withFileTypes: true });
 
-    return files.map((file) => ({
-      name: file.name,
-      type: file.isFile() ? "file" : "directory",
-    }));
+    const entities = files
+      .map((file) => ({
+        name: file.name,
+        type: file.isFile() ? "file" : "directory",
+      }))
+      .reduce(
+        (result, file) =>
+          file.type === "file"
+            ? { ...result, files: [...result.files, file] }
+            : { ...result, directories: [...result.directories, file] },
+        {
+          files: [],
+          directories: [],
+        }
+      );
+    return [...entities.directories.sort(), ...entities.files.sort()];
   } catch (err) {
     if (err.code === ENOENT) {
       throw new Error(ERROR_MESSAGE);
